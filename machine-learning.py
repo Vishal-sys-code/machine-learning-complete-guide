4dc82815b0073ae80cb3cc45a792f9fc9b220cc1
Vishal Pandey <68536727+Vishal-sys-code@users.noreply.github.com>
2025-01-06 23:07:30 +0530

    Update machine-learning.py

--- machine-learning.py
+++ machine-learning.py
@@ -43,7 +43,7 @@ df = pd.read_csv('path_of_your_csv_files.csv')
 
 # If you are fetching csv file from the URL
 url = "https://raw.githubusercontent.com/cs109/2014_data/master/countries.csv"
-headers = {"USer-Agent": "Mozilla/5.0 (Macintosh; Intel Max OS X 10.14; rv:66.0) Gecko/20100101 FireFox/66.0"}
+headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Max OS X 10.14; rv:66.0) Gecko/20100101 FireFox/66.0"}
 req = requests.get(url, headers = headers)
 data = StringIO(req.text)
 pd.read_csv(data)
