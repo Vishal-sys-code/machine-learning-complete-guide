50b30f0efbe3068e7034a9bccf323f093db49083
Vishal Pandey <68536727+Vishal-sys-code@users.noreply.github.com>
2025-02-21 21:32:50 +0530

    Update feature_engineering.py

--- Machine Learning Templates/Feature Engineering Template/feature_engineering.py
+++ Machine Learning Templates/Feature Engineering Template/feature_engineering.py
@@ -15,3 +15,15 @@ from sklearn.model_selection import train_test_split
 ##################################
 
 # Step 1: Missing Value Imputation
+
+'''
+-------- Contents --------
+1.1 Handling Mixed Variables
+1.2 Handling Date and Time Variables
+1.3 Complete Case Analysis
+1.4 Handling Categorical Variables
+1.5 Handling Numerical Variables
+1.6 Random Sample Imputer, Missing Indicator, Auto Select Imputer, KNN Imputer
+1.7 Iterative Imputer
+'''
+
