8158080d4eaa32aa21fc87139200930079f1607b
Vishal Pandey <68536727+Vishal-sys-code@users.noreply.github.com>
2025-02-21 21:27:41 +0530

    Update feature_engineering.py

---
+++ Machine Learning Templates/Feature Engineering Template/feature_engineering.py
@@ -0,0 +1,16 @@
+##################################
+#  FEATURE ENGINEERING TEMPLATE  #
+##################################
+
+# Importing Libraries
+import numpy as np 
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+
+
+##################################
+#     FEATURE TRANSFORMATION     #
+##################################
+
