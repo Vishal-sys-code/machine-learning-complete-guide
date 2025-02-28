c8d5f465d6348d452cd6f8ce395f87c7432f7832
Vishal Pandey <68536727+Vishal-sys-code@users.noreply.github.com>
2025-02-28 10:25:19 +0530

    Update feature_engineering.py

--- Machine Learning Templates/Feature Engineering Template/feature_engineering.py
+++ Machine Learning Templates/Feature Engineering Template/feature_engineering.py
@@ -26,3 +26,26 @@ from sklearn.model_selection import train_test_split
 '''
 
 # 1.1 Complete Case Analysis
+"""
+In complete case analysis, we find all the missing values in the form of the percentage.
+If we get the missing values then we can drop the rows or columns.
+We always drop the rows if the missing values are less than 5%.
+"""
+
+def complete_case_analysis():
+    # Printing or Displaying the missing values of each column in %
+    missing_values = df.isnull().sum()
+    print("Missing Values are (in %): ")
+    print(missing_values)
+
+    # Selecting the columns with less than 5% missing values
+    cols = [var for var in df.columns if df[var].isnull().mean() < 0.05 and df[var].isnull().mean() > 0]
+    print("Selected columns who have less than 5% missing values: ")
+    print(cols)
+
+    # Applying Complete Case Analysis
+    new_df = df[cols].dropna()
+    print("Old Dataframe Shape: ", df.shape)
+    print("New Dataframe Shape: ", new_df.shape)
+
+    # Visualise the distributions Before and After Complete Case Analysis
